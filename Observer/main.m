f4dd90e9b4015b98933739bb6b831c7b39186376
yuxiaopeng <uxiaopeng@gmail.com>
2015-01-04 09:55:58 +0800

    Initial Commit

---
+++ Observer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Observer
+//
+//  Created by 小鹏 on 15/1/4.
+//  Copyright (c) 2015年 ROC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
